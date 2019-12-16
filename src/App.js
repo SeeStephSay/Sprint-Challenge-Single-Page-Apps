24c837d880c27fdc2672105b63766849205259d3
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-15 21:05:42 -0600

    Revert "Installing ReactStrap"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import Header from './components/Header';
-import { Route, Switch } from 'react-router-dom';
-import WelcomePage from './components/WelcomePage';
-// import LocationsList from './components/LocationsList';
-import CharacterList from './components/CharacterList';
-import SearchForm from './components/SearchForm';
+import React from "react";
+import Header from "./components/Header.js";
+
 
 export default function App() {
-	return (
-		<main>
-			<Header />
-			{/* <Switch> */}
-			<Route exact path='/search' component={SearchForm} />
-			<Route exact path='/' component={WelcomePage} />
-			<Route path='/characters' component={CharacterList} />
-			{/* <Route exact path='/LocationsList' component={LocationsList} /> */}
-			{/* </Switch> */}
-		</main>
-	);
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { Card, CardBody, CardImg, CardText, Button } from 'reactstrap';
 
 export default function LocationCard({ name, type, dimension, residents }) {
-  // return <span>todo: location</span>;
-  return (
-    
-  )
+  return <span>todo: location</span>;
 }
