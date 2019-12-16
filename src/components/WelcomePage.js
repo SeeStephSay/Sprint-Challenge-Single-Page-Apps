d618a8c3806992abebcb4e2aed45db4e5470fa56
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-15 23:27:43 -0600

    Added NavBar, and played around with some styling.

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 
-export default function WelcomePage() {
+export default function WelcomePage(props) {
 	return (
 		<section className='welcome-page'>
 			<header>
-				<h1>"Yeah, welcome to the club, pal." - Butter Robot</h1>
+				<Container className='themed-container' fluid={true}>
+					<h3 class='row justify-content-center'>"Yeah, welcome to the club, pal." - Butter Robot</h3>
+				</Container>
 				<img className='main-img' src='https://rickandmortyapi.com/api/character/avatar/1.jpeg' alt='rick' />
 				<div class='row justify-content-center'>
-					<Link to='/characters'>
+					{/*<Link to='/characters'>
 						<Button color='info'>They're Real "Characters"</Button>
-					</Link>
+  </Link>*/}
 				</div>
 			</header>
 		</section>
