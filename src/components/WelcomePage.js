5df97e1d317c9235de1e178130e686dfe9437cee
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-15 22:38:28 -0600

    Installed all dependencies, and began setup with ReactStrap.

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 export default function WelcomePage() {
-  return (
-    <section className="welcome-page">
-      <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
-      </header>
-    </section>
-  );
+	return (
+		<section className='welcome-page'>
+			<header>
+				<h1>"Yeah, welcome to the club, pal." - Butter Robot</h1>
+				<img className='main-img' src='https://rickandmortyapi.com/api/character/avatar/1.jpeg' alt='rick' />
+				<div class='row justify-content-center'>
+					<Link to='/characters'>
+						<Button color='info'>They're Real "Characters"</Button>
+					</Link>
+				</div>
+			</header>
+		</section>
+	);
 }
